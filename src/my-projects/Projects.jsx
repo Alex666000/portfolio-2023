bb16cb32f97698e3e6d85845e8437ac0d52af7bf
Aleksandr_UB <bogdanov777000@mail.ru>
2023-02-11 21:13:03 +0300

    2 занятие начало до медиазапросов--

--- src/my-projects/Projects.jsx
+++ src/my-projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './MyProjects.module.css';
+import s from './Projects.module.css';
 import commonContainer from '../common/styles/commonContainer.module.css';
 import Project from "./project/Project";
 
